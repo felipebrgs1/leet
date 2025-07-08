88fc25cd3c90b4bd63cfc0b1f001df7e025b6cca
felipebrgs <felipeborgaco@hotmail.com>
2025-07-08 15:53:22 -0300

    .

--- CrackingTheCode/isUnique.ts
+++ CrackingTheCode/isUnique.ts
@@ -1,20 +1,16 @@
-function isUnique(Text: string): boolean {
-  if (Text.length > 256) {
-    //caso passe é garantido que tenha um repetido ASCII tem 256
+function isPermutation(str1: string, str2: string): boolean {
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+
+  if (str1.length !== str2.length) {
     return false;
   }
-  let arr = [];
-  let temp: unknown;
-  for (let i = 0; i < Text.length; i++) {
-    temp = Text[i];
-    if (arr.includes(temp)) {
-      return false;
-    }
-    arr.push(temp);
-    //console.log(arr);
-  }
 
-  return true;
+  const sorted1 = str1.split('').sort().join('');
+  const sorted2 = str2.split('').sort().join('');
+
+  return sorted1 === sorted2;
 }
 
-console.log(isUnique("tesd"));
+console.log(isPermutation("fouci", "fuoca")); // false
+console.log(isPermutation("fouci", "cifou")); // true
